277a38e2c3643851cd4ae25caa9ab18da1a60c4b
Beni9 <andre.benea@gmail.com>
2023-12-03 17:57:16 +0200

    added functionality to deal with multiple invoices

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import xml.etree.ElementTree as ET
 from openpyxl import Workbook
@@ -12,7 +13,11 @@ excel_data = []
 def parsing_function(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
-    create_excel(root)
+
+    supplier_data_parsing(root)
+    invoice_general_data(root)
+    customer_data_parsing(root)
+    invoice_items(root)
 
 
 def supplier_data_parsing(root):
@@ -22,12 +27,9 @@ def supplier_data_parsing(root):
     for supplier_data in supplier_party_data:
         excel_data.append(supplier_data.find('.//cbc:RegistrationName', namespace).text)
         supplier_tax_scheme = supplier_data.find('.//cac:TaxScheme/cbc:ID', namespace)
-        supplier_identification_country = supplier_data.find('.//cac:Country/cbc:IdentificationCode', namespace)
-        supplier_company_ID = supplier_data.find(".//cac:PartyLegalEntity/cbc:CompanyID", namespace)
+        supplier_identification_code = supplier_data.find('.//cac:PartyTaxScheme/cbc:CompanyID', namespace)
 
-    supplier_identification_code = supplier_identification_country.text + supplier_company_ID.text
-
-    supplier_identification_number = f"{supplier_identification_code} ({supplier_tax_scheme.text})"
+    supplier_identification_number = f"{supplier_identification_code.text} ({supplier_tax_scheme.text})"
 
     excel_data.append(supplier_identification_number)
 
@@ -38,9 +40,12 @@ def customer_data_parsing(root):
     for customer_data in customer_party_data:
         excel_data.append(customer_data.find('.//cbc:RegistrationName', namespace).text)
         customer_identification_country = customer_data.find('.//cac:Country/cbc:IdentificationCode', namespace)
-        customer_company_ID = customer_data.find(".//cac:PartyLegalEntity/cbc:CompanyID", namespace)
+        customer_company_ID = customer_data.find(".//cbc:CompanyID", namespace)
 
-    customer_identification_code = customer_identification_country.text + customer_company_ID.text
+    if not customer_company_ID.text.startswith("RO"):
+        customer_identification_code = customer_identification_country.text + customer_company_ID.text
+    else:
+        customer_identification_code = customer_company_ID.text
     customer_tax_data = root.findall('.//cac:TaxTotal', namespace)
 
     for tax_data in customer_tax_data:
@@ -72,11 +77,13 @@ def invoice_items(root):
         item_total_full = f"{item_total_price.text} {item_total_currency}"
         item_tva = round(float(item_total_price.text) * (float(item_percentage.text) / 100), 2)
 
+        item_tva_full = str(item_tva) + " " + item_price_currency
         item_data = [item_name.text, item_percentage.text, item_quantity_full,
-                     item_price_full, item_total_full, item_tva]
+                     item_price_full, item_total_full, item_tva_full]
         if (first):
             excel_data[start_index_item:start_index_item] = item_data
             ws.append(excel_data)
+            excel_data.clear()
             first = False
         else:
             invoice_list.append(item_data)
@@ -87,14 +94,16 @@ def invoice_items(root):
 
 
 def invoice_general_data(root):
-    excel_data.append(root.find(".//cbc:IssueDate", namespace).text)
     excel_data.append(root.find(".//cbc:ID", namespace).text)
+    excel_data.append(root.find(".//cbc:IssueDate", namespace).text)
     # Lipseste data incarcarii
     excel_data.append("")
 
 
-def create_excel(root):
+def create_excel():
     # Create a new Excel workbook
+
+    global wb
     wb = Workbook()
     global ws
     ws = wb.active
@@ -111,13 +120,20 @@ def create_excel(root):
     second_row_headers = list(map(str, range(1, len(first_row_headers) + 1)))
     ws.append(second_row_headers)
 
-    supplier_data_parsing(root)
-    invoice_general_data(root)
-    customer_data_parsing(root)
-    invoice_items(root)
+
+def parsing_all_xml_files(folder_path):
+    # Iterate through files in the specified folder
+    create_excel()
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.xml'):  # Process only XML files
+            file_path = os.path.join(folder_path, filename)
+            print(f"Processing {filename}...")
+
+            # Call your XML processing function here
+            parsing_function(file_path)
 
     wb.save('invoice_data.xlsx')
     print("Data extracted and saved to 'invoice_data.xlsx'")
 
 
-parsing_function('P_97301_2023-09-28_651a9709cb19957679aebb23_Factura.xml')
+parsing_all_xml_files('facturi')
