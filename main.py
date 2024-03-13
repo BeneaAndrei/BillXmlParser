1fb3fd25ee2d63bb26e95779db36940c6afeaa53
Beni9 <andre.benea@gmail.com>
2024-03-13 20:29:07 +0200

    Update error handling

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import ctypes
 import os
 import sys
 import xml.etree.ElementTree as ET
 from openpyxl import Workbook
+from tkinter import  Tk, messagebox
+from tkinter.ttk import Progressbar, Label
 
 # Define the namespace used in XML
 namespace = {'cbc': 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2',
              'cac': 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2'}
 
 excel_data = []
+
+def show_message_box(message, type):
+    root = Tk()
+    root.withdraw()  # Hide the root window
+
+    # Show a message box with the given message
+    if type == 'e':
+        messagebox.showwarning("Error", message)
+    if type == "i":
+        messagebox.showinfo("Succes!", message)
 
 
 def parsing_function(xml_file):
@@ -29,7 +42,11 @@ def supplier_data_parsing(root):
         supplier_tax_scheme = supplier_data.find('.//cac:TaxScheme/cbc:ID', namespace)
         supplier_identification_code = supplier_data.find('.//cac:PartyTaxScheme/cbc:CompanyID', namespace)
 
-    supplier_identification_number = f"{supplier_identification_code.text} ({supplier_tax_scheme.text})"
+    if supplier_tax_scheme is not None:
+        supplier_identification_number = f"{supplier_identification_code.text} ({supplier_tax_scheme.text})"
+    else:
+        supplier_tax_scheme = supplier_data.find('.//cac:PartyLegalEntity/cbc:CompanyID', namespace)
+        supplier_identification_number = f"{supplier_tax_scheme.text}"
 
     excel_data.append(supplier_identification_number)
 
@@ -46,12 +63,21 @@ def customer_data_parsing(root):
         customer_identification_code = customer_identification_country.text + customer_company_ID.text
     else:
         customer_identification_code = customer_company_ID.text
+
     customer_tax_data = root.findall('.//cac:TaxTotal', namespace)
 
     for tax_data in customer_tax_data:
         customer_tax_scheme = tax_data.find('.//cac:TaxScheme/cbc:ID', namespace)
 
-    customer_identification_number = f"{customer_identification_code} ({customer_tax_scheme.text})"
+    if customer_tax_scheme is not None and customer_identification_code is not None:
+        customer_identification_number = f"{customer_identification_code}"
+    elif customer_tax_scheme is None and customer_identification_code is not None:
+        customer_identification_number = f"{customer_identification_code.text}"
+    elif customer_identification_code is None and customer_tax_scheme is not None:
+        customer_identification_number = f"{customer_tax_scheme.text}"
+    else:
+        customer_identification_number = f" "
+
     excel_data.append(customer_identification_number)
 
 
@@ -66,7 +92,7 @@ def invoice_items(root):
         item_name = items.find(".//cac:Item/cbc:Name", namespace)
         item_quantity = items.find(".//cbc:InvoicedQuantity", namespace)
         item_quantity_unit = items.find(".//cbc:InvoicedQuantity", namespace).get('unitCode')
-        item_quantity_full = f"{item_quantity.text} {item_quantity_unit}"
+        # item_quantity_full = f"{item_quantity.text} {item_quantity_unit}"
         item_percentage = items.find(".//cac:ClassifiedTaxCategory/cbc:Percent", namespace)
         item_price = items.find(".//cac:Price/cbc:PriceAmount", namespace)
         item_price_currency = items.find(".//cac:Price/cbc:PriceAmount", namespace).get('currencyID')
@@ -74,25 +100,22 @@ def invoice_items(root):
         item_total_price = items.find(".//cbc:LineExtensionAmount", namespace)
         item_total_currency = items.find(".//cbc:LineExtensionAmount", namespace).get('currencyID')
         item_total_full = f"{item_total_price.text} {item_total_currency}"
-        item_tva = round(float(item_total_price.text) * (float(item_percentage.text) / 100), 2)
 
-        item_tva_full = str(item_tva) + " " + item_price_currency
-        item_data = [item_name.text, item_percentage.text, item_quantity_full,
-                     item_price_full, item_total_full, item_tva_full]
+        if item_percentage is not None:
+            item_tva = round(float(item_total_price.text) * (float(item_percentage.text) / 100), 2)
+            item_tva_full = str(item_tva) + " " + item_price_currency
+            item_data = [item_name.text, item_percentage.text, item_quantity.text, item_quantity_unit,
+                         item_price_full, item_total_full, item_tva_full]
+
+        else:
+            item_tva_full = "0 RON"
+            item_data = [item_name.text, "0", item_quantity.text, item_quantity_unit,
+                         item_price_full, item_total_full, item_tva_full]
+
         end_index = start_index_item + len(item_data)
         excel_data[start_index_item:end_index] = item_data
         ws.append(excel_data)
     excel_data.clear()
-
-        # if first:
-            #excel_data.clear()
-            #first = False
-        #else:
-            #invoice_list.append(item_data)
-
-    #for item_data in invoice_list:
-     #   row_data = [""] * start_index_item + item_data
-     #   ws.append(row_data)
 
 
 def invoice_general_data(root):
@@ -114,7 +137,7 @@ def create_excel():
     first_row_headers = [
         'Furnizor', 'Cod fiscal furnizor', 'Numar factura', 'Data emiterii', 'Data incarcare',
         'Cumparator', 'Cod fiscal Cumparator', 'Denumire articol', 'Cota TVA', 'Cantitate',
-        'Pret unitar', 'Total net', 'Valoare TVA'
+        'UM', 'Pret unitar', 'Total net', 'Valoare TVA'
     ]
     ws.append(first_row_headers)
 
@@ -125,17 +148,32 @@ def create_excel():
 
 def parsing_all_xml_files(folder_path):
     # Iterate through files in the specified folder
+    error_flag = False
     create_excel()
     for filename in os.listdir(folder_path):
         if filename.endswith('.xml'):  # Process only XML files
             file_path = os.path.join(folder_path, filename)
             print(f"Processing {filename}...")
 
-            # Call your XML processing function here
-            parsing_function(file_path)
+            try:
+                # Call your XML processing function here
+                parsing_function(file_path)
+            except Exception as e:
+                show_message_box("There was an error processing" + file_path + "with the error: " + str(e), 'e')
+                print("\033[91m {}\033[00m".format(
+                    "There was an error processing" + file_path + "with the error: " + str(e)))
+                error_flag = True
+    try:
+        wb.save('invoice_data.xlsx')
+        print("\033[92mData extracted and saved to 'invoice_data.xlsx'\033[00m")
+        if (error_flag == False):
+            show_message_box("All done", 'i')
+        else:
+            show_message_box("Finished with some errors", 'i')
 
-    wb.save('invoice_data.xlsx')
-    print("Data extracted and saved to 'invoice_data.xlsx'")
+    except Exception:
+        print("\033[91m {}\033[00m".format("There was an error, verify if the invoice excel is closed and try again"))
+        show_message_box("There was an error, verify if the invoice excel is closed and try again", 'e')
 
 
-parsing_all_xml_files('facturi')
+parsing_all_xml_files('.')
